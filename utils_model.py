1eb464409581131e5189f0b2b84daa7aabc5041b
onioncccc <48938841+onioncccc@users.noreply.github.com>
2020-11-15 22:05:05 +0800

    Add files via upload

---
+++ utils_model.py
@@ -0,0 +1,74 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class DQN(nn.Module):
+
+    def __init__(self, action_dim, device):
+        super(DQN, self).__init__()
+        self.__conv1 = nn.Conv2d(4, 32, kernel_size=8, stride=4, bias=False)
+        self.__conv2 = nn.Conv2d(32, 64, kernel_size=4, stride=2, bias=False)
+        self.__conv3 = nn.Conv2d(64, 64, kernel_size=3, stride=1, bias=False)
+        self.__fc1 = nn.Linear(64*7*7, 512)
+        self.__fc2 = nn.Linear(512, action_dim)
+        self.__device = device
+
+    def forward(self, x):
+        x = x / 255.
+        x = F.relu(self.__conv1(x))
+        x = F.relu(self.__conv2(x))
+        x = F.relu(self.__conv3(x))
+        x = F.relu(self.__fc1(x.view(x.size(0), -1)))
+        return self.__fc2(x)
+
+    @staticmethod
+    def init_weights(module):
+        if isinstance(module, nn.Linear):
+            torch.nn.init.kaiming_normal_(module.weight, nonlinearity="relu")
+            module.bias.data.fill_(0.0)
+        elif isinstance(module, nn.Conv2d):
+            torch.nn.init.kaiming_normal_(module.weight, nonlinearity="relu")
+
+
+class Dueling_DQN(nn.Module):
+    def __init__(self, num_actions, device):
+        super(Dueling_DQN, self).__init__()
+        self.num_actions = num_actions
+
+        self.conv1 = nn.Conv2d(in_channels=4, out_channels=32, kernel_size=8, stride=4)
+        self.conv2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=4, stride=2)
+        self.conv3 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1)
+
+        self.fc1_adv = nn.Linear(in_features=7*7*64, out_features=512)
+        self.fc1_val = nn.Linear(in_features=7*7*64, out_features=512)
+
+        self.fc2_adv = nn.Linear(in_features=512, out_features=num_actions)
+        self.fc2_val = nn.Linear(in_features=512, out_features=1)
+
+		self.relu = nn.ReLU()
+		self._device = device
+
+    def forward(self, x):
+        batch_size = x.size(0)
+        x = self.relu(self.conv1(x))
+        x = self.relu(self.conv2(x))
+        x = self.relu(self.conv3(x))
+        x = x.view(x.size(0), -1)
+
+        adv = self.relu(self.fc1_adv(x))
+        val = self.relu(self.fc1_val(x))
+
+        adv = self.fc2_adv(adv)
+        val = self.fc2_val(val).expand(x.size(0), self.num_actions)
+
+        x = val + adv - adv.mean(1).unsqueeze(1).expand(x.size(0), self.num_actions)
+        return x
+
+    @staticmethod
+    def init_weights(module):
+        if isinstance(module, nn.Linear):
+            torch.nn.init.kaiming_normal_(module.weight, nonlinearity="relu")
+            module.bias.data.fill_(0.0)
+        elif isinstance(module, nn.Conv2d):
+            torch.nn.init.kaiming_normal_(module.weight, nonlinearity="relu")
